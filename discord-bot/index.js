6cc85d7bc85056724d1c0da6318e7e054b3561d5
Aitegin Akmatbekov <31095932+aitik@users.noreply.github.com>
2021-08-08 03:41:51 -0500

    Update index.js

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
-const client = new Discord.Client();
 
 client.on('message', message => {
 if(message.content.includes('fan')){
